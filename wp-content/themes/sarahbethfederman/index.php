170c31d28a9bf928a01810afa2d097f92108743b
sarahbethfederman <sarahbethfederman@gmail.com>
2014-08-13 19:03:11 -0400

    basic styles for homepage

--- wp-content/themes/sarahbethfederman/index.php
+++ wp-content/themes/sarahbethfederman/index.php
@@ -2,6 +2,30 @@
   get_header();
 ?>
 
+<div class="container">
+  <a class="post" href="#">
+    <img class="post__background" src="wp-content/themes/sarahbethfederman/img/coffee-table.jpg">
+  </a>
+  <a class="post" href="#">
+    <img class="post__background" src="wp-content/themes/sarahbethfederman/img/coffee-table.jpg">
+  </a>
+  <a class="post" href="#">
+    <img class="post__background" src="wp-content/themes/sarahbethfederman/img/coffee-table.jpg">
+  </a>
+  <a class="post" href="#">
+    <img class="post__background" src="wp-content/themes/sarahbethfederman/img/coffee-table.jpg">
+  </a>
+  <a class="post" href="#">
+    <img class="post__background" src="wp-content/themes/sarahbethfederman/img/coffee-table.jpg">
+  </a>
+  <a class="post" href="#">
+    <img class="post__background" src="wp-content/themes/sarahbethfederman/img/coffee-table.jpg">
+  </a>
+  <a class="post" href="#">
+    <img class="post__background" src="wp-content/themes/sarahbethfederman/img/coffee-table.jpg">
+  </a>
+</div>
+
 <?php
   get_footer();
 ?>
--- wp-content/themes/sarahbethfederman/nav.php
+++ wp-content/themes/sarahbethfederman/nav.php
@@ -1,13 +1,17 @@
+  
   <header class="nav-bar">
     <nav>
       <ul class="main-nav">
         <li><a href="#">About</a></li>
+        <li><a href="#">Resume</a></li>
         <li><a href="#"><img class="logo" src="img/logo.png"></a></li>
         <li><a href="#">Contact</a></li>
+        <li><a href="#">Blog</a></li>
         <li class="social">
           <ul>
             <li><a href="#">Github</a></li>
             <li><a href="#">Treehouse</a></li>
+            <li><a href="#">Dribble</a></li>
             <li><a href="#">Twitter</a></li>
             <li><a href="#">LinkedIn</a></li>
           </ul>
