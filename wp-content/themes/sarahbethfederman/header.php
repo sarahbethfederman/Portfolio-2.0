2585d217f95beda8e7265b59abf5336d73227f29
sarahbethfederman <sarahbethfederman@gmail.com>
2014-08-13 11:39:21 -0400

    configured wordpress installed Yoast SEO plugin installed Compass created Wordpress info sass partial

---
+++ wp-content/themes/sarahbethfederman/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Sarah Beth Federman : New Media Design &amp; Development</title>
+  <!-- META DATA -->
+  <meta name="viewport" content="initial-scale=1">
+
+  <!--STYLESHEETS-->
+  <link href="style.css" rel="stylesheet" media="screen">
+  <link href="print.css" rel="stylesheet" media="print">
+  <!--[if IE]>
+	  <link href="ie.css" rel="stylesheet" type="text/css" />
+  <![endif]-->
+</head>
+<body>
