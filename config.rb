8e808fcadae7bd9d16ef718fd505732fd18841b3
sarahbethfederman <sarahbethfederman@gmail.com>
2014-08-13 10:26:31 -0400

    installed compass

---
+++ config.rb
@@ -0,0 +1,21 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "wp-content/themes/sarahbethfederman/"
+sass_dir = "wp-content/themes/sarahbethfederman/scss"
+images_dir = "wp-content/themes/sarahbethfederman/img"
+javascripts_dir = "wp-content/themes/sarahbethfederman/js"
+fonts_dir = "wp-content/themes/sarahbethfederman/font"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+preferred_syntax = :scss
