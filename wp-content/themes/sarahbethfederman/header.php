e1162e5880b91b68eb3f5710cd0180f315dc17cf
Sarah Federman <sarahbethfederman@gmail.com>
2014-10-01 05:45:22 -0400

    fixing css

--- wp-content/themes/sarahbethfederman/header.php
+++ wp-content/themes/sarahbethfederman/header.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="initial-scale=1">
 
   <!--STYLESHEETS-->
+  <script src="//use.typekit.net/fca7vmw.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
   <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet" media="screen">
 
 </head>
